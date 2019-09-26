2094d13a6fa2cf51e3dc07c94951dd6ea422d917
Jaewoong Heo <koolerjaebee@gmail.com>
2019-09-26 10:49:44 +0900

    ASAP | login.py webdriver fix | 1.02

--- login.py
+++ login.py
@@ -5,19 +5,19 @@ options = webdriver.ChromeOptions()
 options.add_argument('--start-maximized')  # 인터넷 창 최대화
 
 # Chrome 웹 드라이버 생성
-driver = webdriver.Chrome('C:/Users/student.M703/login/chromedriver.exe', chrome_options=options)
+driver = webdriver.Chrome('C:/Users/student.M703/site_login/chromedriver.exe', chrome_options=options)
 
 # url 로딩
-driver.get('<URL>')  # site URL
+driver.get('http://edu.ssafy.com/')  # site URL
 
 assert '<title>' in driver.title  # site 타이틀을 보고 확인 (없어도 상관 없음)
 
 
 user_id = driver.find_element_by_id('userId')
-user_id.send_keys('???@???')  # Id를 인자로 받음
+user_id.send_keys('<Id>')  # Id를 인자로 받음
 
 user_pwd = driver.find_element_by_id('userPwd')
-user_pwd.send_keys('???')  # Password를 인자로 받음
+user_pwd.send_keys('<Pwd>')  # Password를 인자로 받음
 
 login_btn = driver.find_element_by_css_selector('.btn.btn-lg.btn-wide.btn-primary')  # site html의 클래스 등을 통해 특정 가능
 login_btn.click()
