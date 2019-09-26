1478d6881fd79f896873381d24dadf14d57195ef
Jaewoong Heo <koolerjaebee@gmail.com>
2019-09-26 10:30:23 +0900

    auto site login program with python 1.00

---
+++ README.md
@@ -0,0 +1,72 @@
+# site login
+## pip list
+- selenium
+
+## install list
+- chromedriver.exe
+
+## howto
+
+![1](C:\Users\student.M703\site_login\screenshots\1.png)
+
+
+
+작업 스케줄러 열기
+
+
+
+![2](C:\Users\student.M703\site_login\screenshots\2.PNG)
+
+
+
+작업 만들기
+
+
+
+![3](C:\Users\student.M703\site_login\screenshots\3.PNG)
+
+
+
+일반 탭
+
+
+
+![4](C:\Users\student.M703\site_login\screenshots\4.PNG)
+
+
+
+트리거 탭 새로 만들기
+
+
+
+![5](C:\Users\student.M703\site_login\screenshots\5.PNG)
+
+
+
+트리거 설정 
+
+원하는 시간 설정 가능합니다.
+
+
+
+![6](C:\Users\student.M703\site_login\screenshots\6.PNG)
+
+
+
+동작 탭 새로 만들기
+
+
+
+![7](C:\Users\student.M703\site_login\screenshots\7.PNG)
+
+
+
+프로그램/스크립트 : pythonw.exe 의 디렉토리
+
+인수 추가 : login.py 의 디렉토리
+
+
+
+## Reference
+
+- ChromeDriver - WebDriver for Chrome https://chromedriver.chromium.org/downloads
---
+++ login.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')  # 인터넷 창 최대화
+
+# Chrome 웹 드라이버 생성
+driver = webdriver.Chrome('C:/Users/student.M703/login/chromedriver.exe', chrome_options=options)
+
+# url 로딩
+driver.get('<URL>')  # site URL
+
+assert '<title>' in driver.title  # site 타이틀을 보고 확인 (없어도 상관 없음)
+
+
+user_id = driver.find_element_by_id('userId')
+user_id.send_keys('???@???')  # Id를 인자로 받음
+
+user_pwd = driver.find_element_by_id('userPwd')
+user_pwd.send_keys('???')  # Password를 인자로 받음
+
+login_btn = driver.find_element_by_css_selector('.btn.btn-lg.btn-wide.btn-primary')  # site html의 클래스 등을 통해 특정 가능
+login_btn.click()
+
+pop_up = driver.find_elements_by_css_selector('.pop-wrap')  # popup 클래스를 통해 확인
+
+if pop_up:  # popup이 있을 시 팝업 지우기
+    not_today = driver.find_element_by_css_selector('.btn.btn-wide.btn-md.btn-default')
+    not_today.click()
